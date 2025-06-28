637e4e16e65c0cbdc054ecb7634ee7242462ee5c
Ritesh Kumar <ritesharun.01@gmail.com>
2025-06-28 21:45:57 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+    const currentYearSpan = document.getElementById('current-year');
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+
+    
+});
